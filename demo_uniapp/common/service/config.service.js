332ca3eb7bac12c11cd247e6ecd728e637c15c68
Wu Fuming 吴福明 <wufuming@chexiang.com>
2022-03-15 11:06:23 +0800

    update uniapp

--- demo_uniapp/common/service/config.service.js
+++ demo_uniapp/common/service/config.service.js
@@ -4,7 +4,7 @@ let BASE_URL = ''
 if (process.env.NODE_ENV == 'development') {
     BASE_URL = 'http://127.0.0.1:8080' // 开发环境
 } else {
-	BASE_URL = 'http://boot.jeecg.org:8080' // 生产环境
+	BASE_URL = 'http://127.0.0.1:8080' // 生产环境
 }
 let staticDomainURL = BASE_URL+ '/sys/common/static';
 
